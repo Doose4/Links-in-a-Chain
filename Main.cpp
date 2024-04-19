23da1501062a023a39bbab50ead4b0891e624f25
Doose4 <157078178+Doose4@users.noreply.github.com>
2024-04-18 22:18:28 -0500

    update code

--- Main.cpp
+++ Main.cpp
@@ -41,13 +41,41 @@ class Node {
 public:
 	ChainLink* node_data;
 	Node* next;
+	void create_node(ChainLink* data, Node* previous_node=nullptr) {
+		if (previous_node != nullptr)
+		{
+			previous_node->next = this;
+		}
+		this->node_data = data;
+		this->next = NULL;
+	}
 };
 
-Node* create_node(ChainLink* data, Node* previous_node) {
-	Node new_node;
-	previous_node->next = &new_node;
-	new_node.node_data = data;
-	return &new_node;
+int list_size(Node* n) {
+	int size = 0;
+	while (n != NULL) {
+		n = n->next;
+		size++;
+	}
+	return size;
+}
+
+void print_list(Node* n, int depth = 0) {
+	if (depth==0|| list_size(n) <= depth)
+	{
+		while (n != NULL) {
+			cout << n->node_data->get_color() << " ";
+			n = n->next;
+		}
+	}
+	else {
+		for (int i = 0; i < depth; i++)
+		{
+			cout << n->node_data->get_color() << " ";
+			n = n->next;
+		}
+	}
+	
 }
 
 int main(){
@@ -56,19 +84,38 @@ int main(){
 	ChainLink link_three = ChainLink(60);
 	ChainLink link_four = ChainLink(100);
 
+	Node head;
+	head.create_node(&link_one);
+	cout << head.node_data->get_color() << endl;
+	Node two;
+	two.create_node(&link_two, &head);
 
-	//cout << head.node_data->get_color() << endl;
-	//cout << head.next->node_data->get_color() << endl;
+	cout << head.node_data->get_color() << endl;
+	cout << two.node_data->get_color() << endl;
+	cout << head.next->node_data->get_color() << endl;
+
+	Node three;
+	three.create_node(&link_three, &two);
+	Node four;
+	four.create_node(&link_four, &three);
+
+	print_list(&head); 
+	cout << endl;
+	print_list(&head,2);
+	cout << endl;
+	print_list(&head, 4);
+	cout << endl;
+	print_list(&head, 6);
 
 	return 0;
 }
 
 //bl = 0
 //r = 1-14
-//o = 28
-//y = 42
-//g = 56
-//b = 70
-//i = 84
-//v = 99
+//o = 15-28
+//y = 29-42
+//g = 43-56
+//b = 57-70
+//i = 71-84
+//v = 85-99
 //w = 100
